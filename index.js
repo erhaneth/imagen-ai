66b4231beb8f83a32119d650cc3ebfcb631ecdf5
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2022-12-10 11:27:20 -0800

    bug

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 6000;
-
-const app = express()
+const app = express();
 
 // Enable body parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//set static folder
-app.use(express.static(path.join(__dirname,'public')))
-app.use('/openai', require('./routes/openaiRoutes'))
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Server started on this port ${port}`))
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
